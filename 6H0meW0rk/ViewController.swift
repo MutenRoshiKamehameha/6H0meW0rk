bd74f9f4194849f0dc312ff08bb5b4b591cc569a
baygeldi <akylbekuulubaygeldi@gmail.com>
2022-12-13 14:23:35 +0600

    Initial Commit

---
+++ 6H0meW0rk/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  6H0meW0rk
+//
+//  Created by Байгелди Акылбек уулу on 13/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
